da0a9cdf0af1eef378f501707df0b4491a7b271c
DanielFerreRobles <daniferrer2003@gmail.com>
2025-10-08 14:41:09 +0000

    logout.php

--- proyecto/agen-bootstrap-main/theme/logout.php
+++ proyecto/agen-bootstrap-main/theme/logout.php
@@ -1,8 +1,16 @@
 <?php
+// Inicia la sesión actual
 session_start(); 
+
+// Elimina todas las variables de sesión
 session_unset(); 
+
+// Destruye completamente la sesión
 session_destroy();
 
+// Redirige al usuario a la página de login.php
 header('Location: login.php');
+
+// Detiene el script para que no se ejecute nada más después de la redirección
 exit;
 ?>
